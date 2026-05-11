b0abfe6027f0959a80cc6c4384bdaf5c6037e0a5
Đặng Viết Hùng <dangviethung.bvt@gmail.com>
2026-05-11 10:11:23 +0700

    Update routes.js

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -45,13 +45,12 @@ import {
   resetPassword,
 } from "./auth.controller.js";
 
-const router = express.Router();
 
 router.post("/forgot-password", forgotPassword);
 
 router.post("/reset-password", resetPassword);
 
-export default router;
+
 
 
 // =====================================
